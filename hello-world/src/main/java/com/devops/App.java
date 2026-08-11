3ad0441d6b882d807b57c3e10a7e591c11e1d804
riya <rriyar1398@gmail.com>
2026-08-11 13:40:04 +0000

    Add File in Azure-proj repository

--- hello-world/src/main/java/com/devops/App.java
+++ hello-world/src/main/java/com/devops/App.java
@@ -1,10 +1,21 @@
 package com.devops;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
 
-    public static void main(String[] args) {
-
-        System.out.println("DevOps CI/CD Pipeline");
-
-    }
+public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
 }
+
+@GetMapping("/hello")
+public String hello() {
+    return "Hello from Azure!";
+  }
+}
+
--- hello-world/src/test/java/com/devops/AppTest.java
+++ hello-world/src/test/java/com/devops/AppTest.java
@@ -1,38 +1,17 @@
 package com.devops;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@Test
+void helloEndpointMessage() {
+    App app = new App();
+
+    assertEquals("Hello from Azure!", app.hello());
 }
+
+}
+
